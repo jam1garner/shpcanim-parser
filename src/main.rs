0f6963ad4e7126f41d5a6728b61a5f37d8e92d4c
jam1garner <jam1.mcleod@hotmail.com>
2019-05-06 13:24:27 -0400

    Reduce unwrap count

--- src/main.rs
+++ src/main.rs
@@ -29,51 +29,55 @@ struct ShanFile {
     animations : Vec<TpcbAnim>
 }
 
-fn read_tpcb_file(file : &mut File, id : u16) -> TpcbAnim {
-    let start = (file.seek(SeekFrom::Current(4)).unwrap() - 4) as u32;
+fn read_tpcb_file(file : &mut File, id : u16) -> std::io::Result<TpcbAnim> {
+    let start = (file.seek(SeekFrom::Current(4))? - 4) as u32;
     let mut section_offsets = [0u32; 2];
-    file.read_u32_into::<LittleEndian>(&mut section_offsets).unwrap();
-    file.seek(SeekFrom::Current(4)).unwrap();
+    file.read_u32_into::<LittleEndian>(&mut section_offsets)?;
+    file.seek(SeekFrom::Current(4))?;
     let mut unk_shorts = [0u16; 2];
     let mut unk_ints = [0u32; 3];
     let mut unk_floats = [0f32; 12];
-    file.read_u16_into::<LittleEndian>(&mut unk_shorts).unwrap();
-    file.read_u32_into::<LittleEndian>(&mut unk_ints).unwrap();
-    file.read_f32_into::<LittleEndian>(&mut unk_floats).unwrap();
-    let unk_c = file.read_u32::<LittleEndian>().unwrap();
+    file.read_u16_into::<LittleEndian>(&mut unk_shorts)?;
+    file.read_u32_into::<LittleEndian>(&mut unk_ints)?;
+    file.read_f32_into::<LittleEndian>(&mut unk_floats)?;
+    let unk_c = file.read_u32::<LittleEndian>()?;
     let mut unk_floats2 = [0f32; 2];
-    file.read_f32_into::<LittleEndian>(&mut unk_floats2).unwrap();
+    file.read_f32_into::<LittleEndian>(&mut unk_floats2)?;
     
-    let key_count = file.read_u32::<LittleEndian>().unwrap();
+    let key_count = file.read_u32::<LittleEndian>()?;
     let mut animation : Vec<TpcbKeyFrame> = vec![];
     for i in 0..key_count {
-        file.seek(SeekFrom::Start((start + section_offsets[0] + (i * 2)) as u64)).unwrap();
-        let frame = file.read_u16::<LittleEndian>().unwrap();
-        file.seek(SeekFrom::Start((start + section_offsets[1] + (i * 0xC)) as u64)).unwrap();
+        file.seek(SeekFrom::Start((start + section_offsets[0] + (i * 2)) as u64))?;
+        let frame = file.read_u16::<LittleEndian>()?;
+        file.seek(SeekFrom::Start((start + section_offsets[1] + (i * 0xC)) as u64))?;
         let mut data = [0u8; 0xC];
-        file.read(&mut data).unwrap();
+        file.read(&mut data)?;
         animation.push(TpcbKeyFrame { frame, data });
     }
 
-    TpcbAnim { id, unk_shorts, unk_ints, unk_floats, unk_c, unk_floats2, animation}
+    Ok(
+        TpcbAnim { id, unk_shorts, unk_ints, unk_floats, unk_c, unk_floats2, animation}
+    )
 }
 
-fn read_shan_file(file_name : &str) -> ShanFile {
-    let mut file = File::open(file_name).unwrap();
-    file.seek(SeekFrom::Start(4)).unwrap();
-    let default_id = file.read_u32::<LittleEndian>().unwrap();
-    let anim_count = file.read_u32::<LittleEndian>().unwrap();
+fn read_shan_file(file_name : &str) -> std::io::Result<ShanFile> {
+    let mut file = File::open(file_name)?;
+    file.seek(SeekFrom::Start(4))?;
+    let default_id = file.read_u32::<LittleEndian>()?;
+    let anim_count = file.read_u32::<LittleEndian>()?;
     let mut animations : Vec<TpcbAnim> = vec![];
     for i in 0..anim_count {
-        file.seek(SeekFrom::Start((0x80 + (i * 4)) as u64)).unwrap();
-        let id = file.read_u16::<LittleEndian>().unwrap();
-        file.seek(SeekFrom::Start((0x80 + (anim_count * 4) + (i * 4)) as u64)).unwrap();
-        let anim_offset = file.read_u32::<LittleEndian>().unwrap();
-        file.seek(SeekFrom::Start(anim_offset as u64)).unwrap();
-        animations.push(read_tpcb_file(&mut file, id));
+        file.seek(SeekFrom::Start((0x80 + (i * 4)) as u64))?;
+        let id = file.read_u16::<LittleEndian>()?;
+        file.seek(SeekFrom::Start((0x80 + (anim_count * 4) + (i * 4)) as u64))?;
+        let anim_offset = file.read_u32::<LittleEndian>()?;
+        file.seek(SeekFrom::Start(anim_offset as u64))?;
+        animations.push(read_tpcb_file(&mut file, id)?);
     }
     
-    ShanFile { default_id, animations}
+    Ok(ShanFile {
+        default_id, animations
+    })
 }
 
 fn main() {
@@ -82,6 +86,6 @@ fn main() {
         eprintln!("No file passed. Usage:\nshpcanim [file]\n");
         return;
     }
-    let shan_file = read_shan_file(&args[1]);
+    let shan_file = read_shan_file(&args[1]).unwrap();
     println!("{:#?}", shan_file);
 }
